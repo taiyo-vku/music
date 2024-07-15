671711f84250aa1439c3591ea1500a4c810ebdff
Taiyo <hackerlord10x@gmail.com>
2024-07-15 18:25:25 +0700

    home screen

--- lib/common/widgets/appbar/appbar.dart
+++ lib/common/widgets/appbar/appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class TaiyoAppBar extends StatelessWidget implements PreferredSizeWidget {
   const TaiyoAppBar({super.key});
@@ -8,6 +9,17 @@ class TaiyoAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: Colors.transparent,
       elevation: 0,
+      leading: const Icon(Icons.grid_view_rounded),
+      actions: [
+        Container(
+          margin: const EdgeInsets.only(right: 20),
+          child: const CircleAvatar(
+            backgroundImage: NetworkImage(
+              'https://www.vpopwire.com/wp-content/uploads/2020/07/son-tung-mtp-no-longer-singer-at-30-vpop.jpg'
+            ),
+          ),
+        ),
+      ],
     );
   }
 
--- lib/views/home/screens/home.dart
+++ lib/views/home/screens/home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:spotify/common/widgets/appbar/appbar.dart';
 import 'package:spotify/utils/constants/colors.dart';
+
+import '../../../utils/constants/sizes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,9 +12,51 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(gradient: TaiyoColors.linearGradient),
-      child: const Scaffold(
+      child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: TaiyoAppBar(),
+        appBar: const TaiyoAppBar(),
+        // TODO : Body
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Welcome',
+                        style: Theme.of(context).textTheme.bodyLarge),
+                    Text(
+                      'Enjoy your favourite music',
+                      style: Theme.of(context)
+                          .textTheme
+                          .headlineMedium!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 5),
+                    const SizedBox(height: TaiyoSizes.spaceBtwSections),
+
+                    // TODO :FORM SEARCH
+                    TextFormField(
+                      decoration: InputDecoration(
+                        isDense: true,
+                        filled: true,
+                        fillColor: TaiyoColors.white,
+                        hintText: 'Search',
+                        hintStyle: Theme.of(context)
+                            .textTheme
+                            .bodyMedium!
+                            .copyWith(color: Colors.grey.shade400),
+                        prefixIcon: Icon(Iconsax.search_normal,
+                            color: Colors.grey.shade400),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
